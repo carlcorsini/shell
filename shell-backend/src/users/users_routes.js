0fff6d367b9b270415d75451b9338f6b592220da
Carl Corsini <carl.m.corsini@gmail.com>
2019-01-14 09:32:28 -0800

    removed unused route

--- shell-backend/src/users/users_routes.js
+++ shell-backend/src/users/users_routes.js
@@ -4,7 +4,6 @@ const ctrl = require('./users_controller')
 
 router.get('/', ctrl.getAll)
 router.get('/:id', ctrl.getById)
-router.get('/:attr', ctrl.getById)
 router.post('/login', ctrl.login)
 router.post('/register', ctrl.create)
 router.post('/token', ctrl.token)
